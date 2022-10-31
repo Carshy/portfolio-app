877a6e1605be2b874a3e6cc858bed5c31c50a317
Collins <mussocollins@gmail.com>
2022-10-31 22:33:43 +0300

    Add header

---
+++ src/container/About/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './About.scss';
+
+const About = () => (
+  <div>
+    <h1>About</h1>
+  </div>
+);
+
+export default About;
---
+++ src/container/Testimonials/Testimonials.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Testimonials.scss';
+
+const Testimonials = () => (
+  <div />
+);
+
+export default Testimonials;
