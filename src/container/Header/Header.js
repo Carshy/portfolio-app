a64627a24db9538fd4e869831b48dc16b325b2e6
Collins <mussocollins@gmail.com>
2022-11-04 22:02:18 +0300

    Add Header motions and styles

--- src/container/Header/Header.js
+++ src/container/Header/Header.js
@@ -3,8 +3,20 @@ import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Header.scss';
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    x: [100, 0],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
+
 const Header = () => (
-  <div className="app__header app__flex">
+  <div id="home-header" className="app__header app__flex">
     <motion.div
       whileInView={{ x: [-100, 0], opacity: [0, 1] }}
       transition={{ duration: 0.5 }}
@@ -27,8 +39,8 @@ const Header = () => (
     </motion.div>
 
     <motion.div
-      whileInView={{ opacity: [0, 1] }}
-      transition={{ duration: 0.5, delayChildren: 0.5 }}
+      whileInView={{ scale: [0, 1], opacity: [0, 1] }}
+      transition={{ duration: 1, ease: 'easeInOut' }}
       className="app__header-img"
     >
       <img src={images.profile} alt="profile_bg" />
@@ -39,6 +51,18 @@ const Header = () => (
         alt="profile_circle"
         className="overlay_circle"
       />
+    </motion.div>
+
+    <motion.div
+      variant={scaleVariants}
+      whileInView={scaleVariants.whileInView}
+      className="app__header-circles"
+    >
+      {[images.flutter, images.redux, images.sass].map((circle) => (
+        <div className="circle-cmp app__flex" key={`circle-${circle.id}`}>
+          <img src={circle} alt="circles" />
+        </div>
+      ))}
     </motion.div>
   </div>
 );
